90c9aa0ceee072ade3857a9146da2b3315ff5ebf
saasbook <saasbook@saasbook.(none)>
2013-02-07 12:39:26 -0800

    cambio del jueves

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -27,4 +27,15 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  listaRating=rating_list.split(', ')
+
+listaRating.each do |field|
+	if (uncheck==true) 
+	  uncheck(field)
+	else 
+	  check(field)
+	end
+
+	end 
+
 end
