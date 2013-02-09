17d1cc3746e0a7d67fe3a4176b1b9d5109d6fbd4
saasbook <saasbook@saasbook.(none)>
2013-02-09 07:12:00 -0800

    cambios en el ejerccio de pruebas con cucumber

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -23,19 +23,62 @@ end
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+Given /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
   listaRating=rating_list.split(', ')
 
 listaRating.each do |field|
-	if (uncheck==true) 
-	  uncheck(field)
+	if (uncheck=="un") 
+	  uncheck("ratings_"+field)
 	else 
-	  check(field)
+	  check("ratings_"+field)
 	end
 
 	end 
 
+end
+
+When /^(?:|I )press "([^a+])"$/ do |button|
+  click_button(button)
+end
+Then /^(?:|I )should see "\/([^\/]*)\/"$/ do |regexp|
+  regexp = Regexp.new(regexp)
+
+  if page.respond_to? :should
+    page.should have_xpath('//*/<td>', :text => regexp)
+  else
+    assert page.has_xpath?('//*/<td>', :text => regexp)
+  end
+end
+Then /^(?:|I )should not see "\/([^\/]*)\/"$/ do |regexp|
+  regexp = Regexp.new(regexp)
+
+  if page.respond_to? :should
+    page.should have_no_xpath('//*', :text => regexp)
+  else
+    assert page.has_no_xpath?('//*', :text => regexp)
+  end
+end
+
+Then /^the "([^"]*)" field(?: within (.*))? should contain "([^"]*)"$/ do |field, parent, value|
+  with_scope(parent) do
+    field = find_field(field)
+    field_value = (field.tag_name == 'textarea') ? field.text : field.value
+    if field_value.respond_to? :should
+      field_value.should =~ /#{value}/
+    else
+      assert_match(/#{value}/, field_value)
+    end
+  end
+end
+
+Then /^(?:|I )should see (\d+) rows$/ do |nFilas|
+  if page.respond_to? :should
+     page.all('table#movies tr').count.should == Integer(nFilas)+1
+    
+  else
+    page.all('table#movies tr').count.should == Integer(nFilas)+1
+  end
 end
